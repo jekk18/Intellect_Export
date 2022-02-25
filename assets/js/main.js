4d780d0e0e5a97c990cefb25c4506ff1029fd91c
Jeko Tchelidze <jekotchelidze@gmail.com>
2022-02-25 17:42:07 +0400

    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,3 +114,12 @@ $('.main-slider').slick({
     }
 
  });
+
+ $(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#searchList .search-results").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
